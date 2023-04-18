6c76e4054e039e31c38a8c83137c7df3f8b684bc
Djura Neskovic <neskovic991@gmail.com>
2023-04-18 20:51:02 +0200

    Add fetch request wih POST method, updated db.json

--- src/Create.js
+++ src/Create.js
@@ -4,14 +4,26 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('');
-  const [isPending, setIPending] = useState(false);
+  const [isPending, setIsPending] = useState(false);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = {title, body, author};
+    setIsPending(true);
 
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then(() => {
+      setIsPending(false);
+    })
+  }
 
   return (
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="title">Blog title:</label>
         <input 
           type="text"
@@ -36,6 +48,8 @@ const Create = () => {
           value={author}
           onChange={e => setAuthor(e.target.value)}
         />
+        {!isPending && <button className="create__button" type="submit">Submit</button>}
+        {isPending && <button className="create__button" disabled>Adding blog...</button>}
       </form>
     </div>
   );
