694bc49bb9e7e90dc2d13882b5578ce54a36d7bf
Djura Neskovic <neskovic991@gmail.com>
2023-04-18 20:38:46 +0200

    Capture input values onChange

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,10 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('');
-  
+  const [isPending, setIPending] = useState(false);
+
+
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
@@ -14,18 +17,24 @@ const Create = () => {
           type="text"
           id="title"
           required
+          value = {title}
+          onChange={e => setTitle(e.target.value)}
         />
         <label htmlFor="body">Blog text:</label>
         <textarea 
           name="blog body" 
           id="body"
           required
+          value={body}
+          onChange={e => setBody(e.target.value)}
         ></textarea>
         <label htmlFor="author">Blog author:</label>
         <input 
           type="text"
           id="author"
           required
+          value={author}
+          onChange={e => setAuthor(e.target.value)}
         />
       </form>
     </div>
