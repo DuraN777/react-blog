460453e26edf4e871d03152ae4cf1e76338e5bc4
Djura Neskovic <neskovic991@gmail.com>
2023-04-18 20:31:54 +0200

    add Create component link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import Navbar from "./Navbar";
+import Create from "./Create";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="content">
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="create" element={ <Create/>}></Route>
       </Routes>
       </div>
     </div>
