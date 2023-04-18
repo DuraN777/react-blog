610cf7b3b94a13d69890657968825b4533093b0f
Djura Neskovic <neskovic991@gmail.com>
2023-04-18 21:18:56 +0200

    Making  a link to BlogDetails

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({blogs, title}) => {
   return (
     <div className="blog-list">
       <h2 className="blog-list__title">{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-list__item" key={blog.id}>
-          <h3 className="blog-list__item-title">{blog.title}</h3>
-          <p className="blog-list__item-author">Written by {blog.author}</p>
+          <Link to= {`/blogs/${blog.id}`}>
+            <h3 className="blog-list__item-title">{blog.title}</h3>
+            <p className="blog-list__item-author">Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
